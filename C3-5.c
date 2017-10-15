2c86bed2374766e3300482d7883aa23f1bcd03bb
darkglimmer <2392897209@qq.com>
2017-10-15 10:52:35 +0800

    C123

--- C3-5.c
+++ C3-5.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-int main() {
-    printf("\n");
+int main(void) {
+    int a;
+    printf("输入一个整数\n");scanf("%d",&a);
+    if (a==0)
+        puts("该整数是零");
+    else
+        puts("该整数不是零");
     return 0;
 }
