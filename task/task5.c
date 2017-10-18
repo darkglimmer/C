9fff07d9bccecba0897fcfade9ec9e9061429a47
darkglimmer <2392897209@qq.com>
2017-10-19 00:26:11 +0800

    task

--- task/task5.c
+++ task/task5.c
@@ -3,20 +3,26 @@
 int main() {
     int i,j,no;
     int count=0;
-        for ( i = 2,no=0; i <=100; i++)
+    for ( i = 2; i <=100; i++)
+    {
+        no=0;
+        for ( j = 2; j < i; j++)
         {
-            no=0;
-            for ( j = 2; count=0,j < i; j++)
-            {
-                if((i%j)==0)
-                    count++;
-                    no++;
-            }
-            if(no==0)
-                printf(" %d ",i );
-            if(count%5==0)
-                printf("\n");
+            if((i%j)==0)
+                no++;
         }
-        return 0;
+        if(no==0)
+        {
+            count++;
+            if (count%5==0)
+            {
+                printf(" %d\n ",i );
+            }
+            else
+            {
+                printf(" %d ",i );
+            }
+        }
     }
-
+    return 0;
+}
